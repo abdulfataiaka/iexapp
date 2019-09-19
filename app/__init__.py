89c311a47a551452faca318c33a844fe3397f1ff
abdulfataiaka <abdulfataiaka@gmail.com>
2019-09-19 23:38:05 +0100

    Setup database for application
    - install flask sqlalchemy
    - add user model with tablename users
    - install python dotenv
    
    [Finishes]

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
+from config.db import DB
+from cli import dbsetup
+from dotenv import load_dotenv
 from flask import Flask, jsonify
 from .controllers.auth import bp as auth
 
-def create_app():
-    app = Flask(__name__)
+load_dotenv()
 
+def create_app(config=None):
+    app = Flask(__name__)
     app.register_blueprint(auth)
+
+    # setting up database
+    DB.init(app)
+    app.cli.add_command(dbsetup)
 
     @app.route('/')
     def root():
