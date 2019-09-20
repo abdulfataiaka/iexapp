31f425821611fb0f4035d51e5204226640ffcd6d
abdulfataiaka <abdulfataiaka@gmail.com>
2019-09-20 16:54:03 +0100

    Implement getting purchased user stocks
    - add endpoint for get user stocks
    - add test for implementation
    
    [Finishes]

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -1,6 +1,15 @@
 from config.db import db
+from app.models.user_stock import UserStock
 from flask import Blueprint, request, jsonify
 from app.middlewares.auth_middleware import AuthMiddleware
 
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+
+@bp.route('/stocks', methods=['GET'])
+@AuthMiddleware.verify_token
+def user_stocks():
+    stocks = UserStock.query.filter_by(user_id=request.user.id).all()
+    stocks = [stock.serialize() for stock in stocks]
+    return jsonify(dict(message='User stocks fetch successfully', stocks=stocks)), 200
