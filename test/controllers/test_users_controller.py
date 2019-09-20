fba02558e8b212f1ef1f59e04e6f5d22c3130e61
abdulfataiaka <abdulfataiaka@gmail.com>
2019-09-20 17:04:05 +0100

    Implement get user transactions
    - add endpoint to geet transactions
    - add test for endpoint
    
    [Finishes]

--- test/controllers/test_users_controller.py
+++ test/controllers/test_users_controller.py
@@ -10,3 +10,11 @@ class TestUsersController(BaseTest):
         response = self.client.get('/users/stocks', headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertTrue('stocks' in response.json)
+
+    def test_user_transactions(self):
+        self.create_user(username='newuser', password='password')
+        token = Helper.token(1, 'newuser')
+        headers = dict(Token=token)
+        response = self.client.get('/users/transactions', headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('transactions' in response.json)
